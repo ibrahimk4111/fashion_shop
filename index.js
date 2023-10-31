96677277d85660f679f00745e4dc6f7b38b611fb
ibrahimk4111 <ibrahimk4111@gmail.com>
2023-10-31 17:41:19 +0600

    first

---
+++ index.js
@@ -0,0 +1,7 @@
+function videoAnimation() {
+    const videoContainer = document.getElementById('video-container')
+    const playBtn = document.getElementById('play')
+    playBtn.addEventListener("mouseenter", function(){
+        gsap.to()
+    })
+}
